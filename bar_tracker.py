90755c7d4f1a9139dccc0d575170cf5d5d1b4c1b
Will Grant <wpg23@cam.ac.uk>
2019-05-03 17:29:21 +0100

    fridays progress on bartracker

--- bar_tracker.py
+++ bar_tracker.py
@@ -174,7 +174,6 @@ if __name__ == '__main__':
             # Tracking success
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            
             cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
         else:
             # Tracking failure
