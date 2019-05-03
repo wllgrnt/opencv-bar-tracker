c42e1cdf953a0b7b7a2983e6e391e17b7f163a54
Will Grant <wpg23@cam.ac.uk>
2019-05-03 16:11:19 +0100

    bar tracker running up against opencv docs

---
+++ bar_tracker.py
@@ -0,0 +1,200 @@
+import os
+import cv2
+import sys
+import numpy as np
+import tensorflow as tf
+
+# Import utilites from research/object_detection
+# requires that this be on the PYTHONPATH
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+
+def getInitialBoundingBox(frame):
+    """
+    given an image, find the bounding box for the barbell plate using the
+    trained RCNN.
+
+    This is then tracked using OpenCV KCF.
+    """
+    # Name of the directory containing the object detection module we're using
+    MODEL_NAME = 'inference_graph'
+    VIDEO_NAME = sys.argv[1]
+
+    # Grab path to current working directory
+    CWD_PATH = os.getcwd()
+
+    # Path to frozen detection graph .pb file, which contains the model that is used
+    # for object detection.
+    PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME,
+                                'frozen_inference_graph.pb')
+
+    # Path to label map file
+    PATH_TO_LABELS = os.path.join(CWD_PATH, 'training', 'labelmap.pbtxt')
+
+    # Number of classes the object detector can identify
+    NUM_CLASSES = 1
+
+    # Load the label map.
+    # Label maps map indices to category names, so that when our convolution
+    # network predicts `5`, we know that this corresponds to `king`.
+    # Here we use internal utility functions, but anything that returns a
+    # dictionary mapping integers to appropriate string labels would be fine
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(
+        label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+    # Load the Tensorflow model into memory.
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+        sess = tf.Session(graph=detection_graph)
+
+    # Define input and output tensors (i.e. data) for the object detection classifier
+
+    # Input tensor is the image
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    # Output tensors are the detection boxes, scores, and classes
+    # Each box represents a part of the image where a particular object was detected
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represents level of confidence for each of the objects.
+    # The score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name(
+        'detection_classes:0')
+
+    # Number of objects detected
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Get the actual frame to analyse
+    frame_expanded = np.expand_dims(frame, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: frame_expanded})
+
+    # Draw the results of the detection (aka 'visulaize the results')
+    xmin, xmax, ymin, ymax = np.squeeze(boxes)[0]
+
+    # Gotta multiply by image width and height to get pixels
+    im_width, im_height, _  = frame.shape
+    xmin = int(xmin*im_width)
+    xmax = int(xmax*im_width)
+    ymin = int(ymin*im_height)
+    ymax = int(ymax*im_height)
+    return (xmin, xmax, ymin, ymax)
+
+if __name__ == '__main__':
+
+    # Set up tracker.
+    tracker_types = [
+        'BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE',
+        'CSRT'
+    ]
+    tracker_type = tracker_types[2]
+    # Bad
+    # if tracker_type == 'BOOSTING':
+    #     tracker = cv2.TrackerBoosting_create()
+    # if tracker_type == 'MIL':
+    #     tracker = cv2. TrackerMIL_create()
+    # if tracker_type == 'KCF':
+    #     tracker = cv2.TrackerKCF_create()
+    # if tracker_type == 'TLD':
+    #     tracker = cv2.TrackerTLD_create()
+    # if tracker_type == 'MEDIANFLOW':
+    #     tracker = cv2.TrackerMedianFlow_create()
+    # if tracker_type == 'GOTURN':
+    #     tracker = cv2.TrackerGOTURN_create()
+    # if tracker_type == 'MOSSE':
+    #     tracker = cv2.TrackerMOSSE_create()
+    # if tracker_type == "CSRT":
+    #     tracker = cv2.TrackerCSRT_create()
+
+    # Read video
+    try:
+        VIDEO_NAME = sys.argv[1]
+    except IndexError:
+        print("please supply a path to a video file")
+        sys.exit()
+
+    video = cv2.VideoCapture(VIDEO_NAME)
+
+    # Exit if video not opened.
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+
+    # Lets write the annotated file out as an mp4
+    # height, width, layers = frame.shape
+    # fps = video.get(cv2.CAP_PROP_FPS)
+    # fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    # video_out = cv2.VideoWriter("out.mp4", fourcc, fps, (width,height), isColor=True)
+
+    # frameCount = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Define an initial bounding box
+    bbox = getInitialBoundingBox(frame)
+    print(bbox)
+
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
+
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+
+        # Start timer
+        timer = cv2.getTickCount()
+
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+        else:
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100, 80),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+
+        # Display result
+        cv2.imshow("Tracking", frame)
+
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27: break
+    
+    # Clean up
+    video.release()
+    cv2.destroyAllWindows()
